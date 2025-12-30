3bc02c1f6a88686286a5e05d0be26a3d4d7f7e9b
Ku0t <kuotthiik@gmail.com>
2025-12-30 20:15:04 +1100

    ecom part 4

---
+++ 3_ktstore/app/page.js
@@ -0,0 +1,14 @@
+import ImageBanner from "@/components/ImageBanner";
+import Products from "@/components/Products";
+
+export default function Home() {
+
+  return (
+    <>
+      <ImageBanner />
+      <section>
+        <Products />
+      </section>
+    </>
+  );
+}
