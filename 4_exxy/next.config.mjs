d1fe0ec7d3e0709769ee03e14b75f9f27a414163
Ku0t <kuotthiik@gmail.com>
2026-01-17 20:30:15 +1100

    fixing deploy issues

--- 4_exxy/next.config.mjs
+++ 4_exxy/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  // REQUIRED for Netlify static hosting
+  output: "export",
+
+  // REQUIRED if you use next/image
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
